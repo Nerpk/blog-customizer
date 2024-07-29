dcfb5851aef0fd92f6db37da1572077167d77cd0
Nerpk <nerpochkagolub@yandex.ru>
2024-07-29 11:03:12 +0300

    functional on react

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -1,20 +1,146 @@
+import styles from './ArticleParamsForm.module.scss';
+import React, { useEffect, useRef, useState } from 'react';
+
+//константы
+import { OptionType, TSettingsArticle, firstSettings, backgroundColors, contentWidthArr, fontColors, fontFamilyOptions, fontSizeOptions } from 'src/constants/articleProps';
+
+//элементы из проекта
 import { ArrowButton } from 'components/arrow-button';
 import { Button } from 'components/button';
+import { Select } from '../select';
+import { RadioGroup } from '../radio-group';
+import { Separator } from '../separator';
+import { Text } from '../text'
 
-import styles from './ArticleParamsForm.module.scss';
+export const ArticleParamsForm = (props: {onClick: (settings: TSettingsArticle)=>void}) => {
+	//открытие закрытие формы
+	const [isOpen, setIsOpen] = useState(false);
+	const refForm = useRef<HTMLFormElement | null>(null);
+	const toggleForm = () => {
+		setIsOpen(!isOpen);
+	};
+	useEffect(() => {
+	  const handleClickOutside = (e: MouseEvent) => {
+		if (refForm.current && !e.composedPath().includes(refForm.current) && isOpen) {
+			toggleForm()
+		}
+	  };
+  
+	  document.addEventListener('click', handleClickOutside);
+	  return () => {
+		document.removeEventListener('click', handleClickOutside);
+	  };
+	}, [isOpen]);
 
-export const ArticleParamsForm = () => {
+	//обновление страницы
+	const settings = (): TSettingsArticle => {
+		return {
+			font: selectedFontOption,
+			fontSize: selectedSizeFontOption,
+			fontColor: selectedColorFontOption,
+			backgroundColor: selectedColorBGOption,
+			width: selectedContentWidthOption,
+		}
+	}
+	const resetSettings = (): TSettingsArticle => {
+		event?.preventDefault()
+
+		handleFontOptionChange(firstSettings.font);
+		handleSizeFontOptionChange(firstSettings.fontSize);
+		handleSizeColorOptionChange(firstSettings.fontColor);
+		handleColorBGOptionChange(firstSettings.backgroundColor);
+		handleContentWidthOptionChange(firstSettings.width);
+
+		//toggleForm();           //опционально: если нужно закрывать форму после изменений, убрать коментирование
+
+		props.onClick(firstSettings)
+		return firstSettings;
+	}
+	const applySettings = () => {
+		event?.preventDefault()
+		//toggleForm();           //опционально: если нужно закрывать форму после изменений, убрать коментирование
+		props.onClick(settings())
+		return settings();
+	}
+	
+	//внутренность формы
+	const [selectedFontOption, setSelectedFontOption] = useState<OptionType>(firstSettings.font);
+	const handleFontOptionChange = (option: OptionType) => {
+		setSelectedFontOption(option);
+	};
+
+	const [selectedSizeFontOption, setSelectedSizeFontOption] = useState<OptionType>(firstSettings.fontSize)
+	const handleSizeFontOptionChange = (option: OptionType) => {
+		setSelectedSizeFontOption(option);
+	};
+
+	const [selectedColorFontOption, setSelectedColorFontOption] = useState<OptionType>(firstSettings.fontColor);
+	const handleSizeColorOptionChange = (option: OptionType) => {
+		setSelectedColorFontOption(option);
+	};
+
+	const [selectedColorBGOption, setSelectedColorBGOption] = useState<OptionType>(firstSettings.backgroundColor);
+	const handleColorBGOptionChange = (option: OptionType) => {
+		setSelectedColorBGOption(option);
+	};
+
+	const [selectedContentWidthOption, setSelectedContentWidthOption] = useState<OptionType>(firstSettings.width);
+	const handleContentWidthOptionChange = (option: OptionType) => {
+		setSelectedContentWidthOption(option);
+	};
+
 	return (
-		<>
-			<ArrowButton />
-			<aside className={styles.container}>
-				<form className={styles.form}>
-					<div className={styles.bottomContainer}>
-						<Button title='Сбросить' type='reset' />
-						<Button title='Применить' type='submit' />
-					</div>
-				</form>
-			</aside>
-		</>
+	  <>
+		<ArrowButton onClick={toggleForm} isOpen={isOpen} />
+		<aside className={[styles.container, isOpen?styles.container_open:''].join(' ')} >
+		  <form className={styles.form} ref={refForm}>
+		  <Text as="h2" size={31} uppercase weight={800}>Задайте параметры</Text>
+
+			<Select 
+				selected={selectedFontOption} 
+				options={fontFamilyOptions}
+				placeholder='Выберите шрифт'
+				title='Шрифт'
+				onChange={handleFontOptionChange}
+			/>
+			<RadioGroup 
+				name='Размер шрифта'
+				options={fontSizeOptions}
+				selected={(selectedSizeFontOption as OptionType)}
+				title='Размер шрифта'
+				onChange={handleSizeFontOptionChange}
+			/>
+			<Select 
+				selected={selectedColorFontOption} 
+				options={fontColors}
+				placeholder='Выберите цвет шрифта'
+				title='Цвет шрифта'
+				onChange={handleSizeColorOptionChange}
+			/>
+
+			<Separator />
+
+			<Select 
+				selected={selectedColorBGOption} 
+				options={backgroundColors}
+				placeholder='Выберите цвет фона'
+				title='Цвет фона'
+				onChange={handleColorBGOptionChange}
+			/>
+			<Select 
+				selected={selectedContentWidthOption} 
+				options={contentWidthArr}
+				placeholder='Выберите размер страницы'
+				title='Ширина контента'
+				onChange={handleContentWidthOptionChange}
+			/>
+
+			<div className={styles.bottomContainer}>
+			  <Button title="Сбросить" type="reset" onClick={resetSettings}/>
+			  <Button title="Применить" type="submit" onClick={applySettings}/>
+			</div>
+		  </form>
+		</aside>
+	  </>
 	);
-};
+  };
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,10 @@
 import { createRoot } from 'react-dom/client';
-import { StrictMode, CSSProperties } from 'react';
+import { StrictMode, CSSProperties, useState, useEffect } from 'react';
 import clsx from 'clsx';
 
 import { Article } from './components/article/Article';
 import { ArticleParamsForm } from './components/article-params-form/ArticleParamsForm';
-import { defaultArticleState } from './constants/articleProps';
+import { OptionType, TSettingsArticle, defaultArticleState, firstSettings } from './constants/articleProps';
 
 import './styles/index.scss';
 import styles from './styles/index.module.scss';
@@ -13,19 +13,31 @@ const domNode = document.getElementById('root') as HTMLDivElement;
 const root = createRoot(domNode);
 
 const App = () => {
+	const [settingArticle, setSettingArticle] = useState<TSettingsArticle>(firstSettings)
+	const updateSettings = (settings: TSettingsArticle) => {
+		setSettingArticle({
+			...settingArticle, 
+			font: settings.font,
+			fontSize: settings.fontSize,
+			fontColor: settings.fontColor,
+			backgroundColor: settings.backgroundColor,
+			width: settings.width,
+		})
+	}
+
 	return (
 		<div
 			className={clsx(styles.main)}
 			style={
 				{
-					'--font-family': defaultArticleState.fontFamilyOption.value,
-					'--font-size': defaultArticleState.fontSizeOption.value,
-					'--font-color': defaultArticleState.fontColor.value,
-					'--container-width': defaultArticleState.contentWidth.value,
-					'--bg-color': defaultArticleState.backgroundColor.value,
+					'--font-family': settingArticle.font.value,
+					'--font-size': settingArticle.fontSize.value,
+					'--font-color': settingArticle.fontColor.value,
+					'--container-width': settingArticle.width.value,
+					'--bg-color': settingArticle.backgroundColor.value,
 				} as CSSProperties
 			}>
-			<ArticleParamsForm />
+			<ArticleParamsForm onClick={updateSettings} />
 			<Article />
 		</div>
 	);
